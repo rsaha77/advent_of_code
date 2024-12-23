4fb92e220feabc23e9115c8899620009f4c9deff
Rana <rsaha.nitdgp@gmail.com>
2024-12-23 12:13:48 +0530

    Maximum clique for p2 .. optimize p1 & clean up

--- 2024/23.py
+++ 2024/23.py
@@ -1,23 +1,6 @@
-import re
 import sys
-import copy
-# import numpc
 import networkx as nx
-import math
-# from graphlib import TopologicalSorter
-# from itertools import product, combinations
-# from functools import cache
-# from heapq import heappush as hpush, heappop as hpop
 from collections import deque, defaultdict, Counter
-
-# sys.setrecursionlimit(int(1e6))
-
-""" Transposing
-tr = list(map(list, zip(*G)))
-tr = list(map(list, itertools.zip_longest(*grid, fillvalue=None))) #when one or more lists are empty
-"""
-
-import networkx as nx
 
 
 file = "in.txt"
@@ -31,21 +14,6 @@ inp = open(file).read().strip()
 LINES = inp.split("\n")
 
 
-def keep_alpnum_spaces(text):
-  ret = "".join(ch if ch.isalnum() or ch.isspace() else "" for ch in text)
-  return ret
-
-
-def remove_chars(text, chs):
-  ret = "".join([ch for ch in text if ch not in chs])
-  return ret
-
-
-def show (**kwargs):
-    output = ', '.join([f"{key} = {value}" for key, value in kwargs.items()])
-    print(output)
-
-
 def ins(r, c, R, C):
   return 0 <= r < R and 0 <= c < C
 
@@ -55,42 +23,11 @@ DIAG_DIR = [[1, 1], [-1, -1], [-1, 1], [1, -1]]
 ALL_DIR = HV_DIR + DIAG_DIR
 
 
-def print_grid(mark):
-  R, C = len(mark), len(mark[0])
-  for r in range(R):
-    for c in range(C):
-      print(mark[r][c], end="")
-    print()
-  print()
-
-
-def generate_combinations(n, lst):
-  return [list(combination) for combination in product(lst, repeat=n)]
-
-
-def merge_and_sort_lists(list1, list2, list3):
-    merged_list = list1 + list2 + list3
-    sorted_list = sorted(merged_list, key=lambda x: x[0])
-    return sorted_list
-
-
-def calculate_distance(point1, point2):
-  return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-
-
-directions = ['^', '>', 'v', '<']
-dir_map = {
-  '^': (-1, 0),
-  '>': (0, 1),
-  'v': (1, 0),
-  '<': (0, -1)
-}
-
 def is_connected(a, b, c, d):
   return a in d[b] and b in d[c] and c in d[a]
 
 
-def has_t(a,b,c):
+def startswith_t(a,b,c):
   return a.startswith('t') or b.startswith('t') or c.startswith('t')
 
 
@@ -99,37 +36,27 @@ def get_sorted(a,b,c):
   return sorted(x)
 
 
-def find_max_clique(adj_list):
-  G = nx.Graph(adj_list)
+def find_max_clique(adj_set):
+  G = nx.Graph(adj_set)
   max_clique = max(nx.find_cliques(G), key=len)
   return list(max_clique)
 
 
 def main():
   ans1, ans2 = 0, 0
-  nodes = set()
-  g = defaultdict(list)
-  d = defaultdict(set)
+  g = defaultdict(set)
   for ln, line in enumerate(LINES):
     u, v = line.split('-')
-    nodes.add(u)
-    nodes.add(v)
-    g[u].append(v)
-    g[v].append(u)
-    d[u].add(v)
-    d[v].add(u)
+    g[u].add(v)
+    g[v].add(u)
 
-  nodes = [x for x in nodes]
+  nodes = sorted(g.keys())
 
-  seen = set()
   for i in range(len(nodes)):
-    for j in range(1, len(nodes)):
-      for k in range(2, len(nodes)):
-        a, b, c = get_sorted(nodes[i], nodes[j], nodes[k])
-        if (a,b,c) in seen:
-          continue
-        seen.add((a,b,c))
-        if is_connected (a,b,c,d) and has_t (a,b,c):
+    for j in range(i+1, len(nodes)):
+      for k in range(j+1, len(nodes)):
+        a,b,c = nodes[i], nodes[j], nodes[k]
+        if is_connected(a,b,c,g) and startswith_t(a,b,c):
           ans1 += 1
 
   print("p1: ", ans1)
