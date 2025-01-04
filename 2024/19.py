43faaa7e0424ac8186a0edf5c3aaa0daad624827
Rana <rsaha.nitdgp@gmail.com>
2025-01-05 02:25:28 +0530

    DP word break

--- 2024/19.py
+++ 2024/19.py
@@ -13,44 +13,31 @@ inp = open(file).read().strip()
 LINES = inp.split("\n")
 
 
-dp = {}
-def is_valid_design_obsolete (pat, line):
-  # This function has an issue for part 2, because of repeating checks when checking (line[:i],line[i:])
-  if line in pat:
-    return True
-  if line in dp:
-    return dp[line]
-  is_valid = False
-  for i in range(1, len(line)):
-    is_valid = is_valid or is_valid_design(pat, line[:i]) and is_valid_design (pat, line[i:])
-  dp[line] = is_valid
-  return dp[line]
-
-
-dp = {}
-def total_valid_design (patterns, target):
-  if target in dp:
-    return dp[target]
-
-  if len(target) == 0:
+def solve (idx, s, words, dp):
+  if idx == len(s):
     return 1
 
-  valid_design_cnt = 0
-  for pattern in patterns:
-    if target.startswith(pattern):
-      valid_design_cnt += total_valid_design(patterns, target[len(pattern):])
-  dp[target] = valid_design_cnt
+  if idx in dp:
+    return dp[idx]
+  
+  ret = 0
+  for word in words:
+    if s[idx:].startswith(word):
+      ret = ret + solve(idx + len(word), s, words, dp)
 
-  return dp[target]
+  dp[idx] = ret #ways - starting from index idx
+  return dp[idx]
 
 
 def main():
   ans1, ans2 = 0, 0
   patterns = list(LINES[0].split(', '))
   for idx, line in enumerate(LINES[2:]):
-    tot = total_valid_design (patterns, line)
-    ans1 += 1 if tot > 0 else 0
-    ans2 += tot
+    dp = {}
+    tot = solve (0, line, patterns, dp)
+    if tot > 0:
+      ans1 += 1
+      ans2 += tot
   print("p1: ", ans1)
   print("p2: ", ans2)
 
