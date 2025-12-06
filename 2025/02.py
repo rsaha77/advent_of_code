2e674f956e2f942af61beaf0ac5a708932942f2e
Rana Saha <rsaha.nitdgp@gmail.com>
2025-12-07 03:18:34 +0800

    String periodicity | Refactor p2 to use single line trick

--- 2025/02.py
+++ 2025/02.py
@@ -1,41 +1,18 @@
 import sys
-from pathlib import Path
-
-def read_lines(fn):
-  f = Path(sys.argv[1]) if len(sys.argv) > 1 else Path(fn)
-  return [ln.strip() for ln in f.read_text().splitlines() if ln.strip()]
-
-
-def ok(s, rep):
-  ln = len(s)
-  for r in range(rep):
-    for nxt in range(r+rep, ln, rep):
-      if s[r] != s[nxt]:
-        return False
-  return True
-
 
 def main():
-  lines = read_lines("in.txt")
-  L = lines[0].split(',')
+  line = sys.stdin.read()
   p1, p2 = 0, 0
-  for x in L:
+  for x in line.split(','):
     a,b = map(int,x.split('-'))
     for num in range(a,b+1):
       s = str(num)
       ln = len(s)
       mid = ln//2
-      if ln % 2 == 0 and s[:mid] == s[mid:]:
+      if not ln & 1  and s[:mid] == s[mid:]:
         p1 += num
+      if s in (s + s)[1:-1]:
         p2 += num
-        continue
-      # check rest of slices for part 2
-      for rep in range(1,mid+1):
-        if ln % rep != 0:
-          continue
-        if ok(s, rep):
-          p2 += num
-          break
   print("p1: ", p1)
   print("p2: ", p2)
 
