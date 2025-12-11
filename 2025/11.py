c558550c40f1e78bb3fd7469addd0a2e7eca62f9
Rana Saha <rsaha.nitdgp@gmail.com>
2025-12-12 03:02:38 +0800

    Top Down DP with DFS in DAG | refactor: cleaner solution

--- 2025/11.py
+++ 2025/11.py
@@ -1,42 +1,20 @@
 import sys
 from collections import defaultdict
+from functools import cache
 
-"""
-It's a DAG
-dp[state] = Starting from this state, how many valid paths lead to the target?
-"""
+@cache
+def f(u, dac, fft):
+  dac |= u == "dac"
+  fft |= u == "fft"
+  if u == "out":
+    return dac and fft
+  return sum (f(v,dac,fft) for v in G[u])
 
-def solve(source, G, p1):
-  DP = {}
-  def dfs (u, seen_dac, seen_fft):
-    state = u if p1 else (u, seen_dac, seen_fft)
-    if state in DP:
-        return DP[state]
+data = sys.stdin.read()
+G = defaultdict(list)
+for line in data.splitlines():
+  u,v = line.split(':')
+  G[u] = v.split()
 
-    seen_dac |= (u == "dac")
-    seen_fft |= (u == "fft")
-
-    if u == "out":
-      return 1 if (p1 or (seen_dac and seen_fft)) else 0
-    
-    tot = 0
-    for v in G[u]:
-      tot += dfs (v, seen_dac, seen_fft)
-    DP[state] = tot
-    return tot
-  return dfs(source, False, False)
-
-
-def main():
-  data = sys.stdin.read()
-  G = defaultdict(list)
-  for line in data.splitlines():
-    line = line.split(':')
-    G[line[0]] = line[1].split()
-
-  print("p1: ", solve("you", G, p1=True))
-  print("p2: ", solve("svr", G, p1=False))
-
-
-if __name__ == "__main__":
-  main()
+print("p1: ", f("you",True,True))
+print("p2: ", f("svr",False,False))  
